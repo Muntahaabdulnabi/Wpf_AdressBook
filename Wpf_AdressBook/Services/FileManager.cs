3357ad63083a36769936ec83574827d52ddf0874
muntahaabdulnabi <muntahaabdulnabi@hotmail.com>
2022-10-24 14:07:05 +0200

    Interface med läs och skriv funtioner added

--- Wpf_AdressBook/Services/FileManager.cs
+++ Wpf_AdressBook/Services/FileManager.cs
@@ -1,12 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Wpf_AdressBook.Services
 {
-    internal class FileManager
+    internal interface IFileManager
     {
+        public void Save(String filePath, string content);
+        public string Read(String filePath);
+    }
+
+    internal class FileManager : IFileManager
+    {
+        public string Read(string filePath)
+        {
+            using var sr = new StreamReader(filePath);
+            return sr.ReadToEnd();
+        }
+
+        public void Save(string filePath, string content)
+        {
+            using var sw = new StreamWriter(filePath);
+            sw.Write(content);
+        }
     }
 }
+// Funktionaliteten som behövs 
